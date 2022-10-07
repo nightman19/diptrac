b0da728c86b8048ec49b1dfa8219a27c00b2a8ed
nightman19 <omahnuru2010@gmail.com>
2022-10-07 01:17:56 +0000

    Created customer delivery directory.

---
+++ lib/ui/pages/customers/customer_onboarding_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class CustomerOnboardingPage extends StatefulWidget {
+  const CustomerOnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  State<CustomerOnboardingPage> createState() => _CustomerOnboardingPageState();
+}
+
+class _CustomerOnboardingPageState extends State<CustomerOnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
